dbb0ba7c29d289088dac6c00566d78d160764666
denis909 <denis909@mail.ru>
2020-04-23 00:32:55 +0300

    n/a

--- components/StorageUploadBehavior.php
+++ components/StorageUploadBehavior.php
@@ -2,9 +2,66 @@
 
 namespace denis909\storage\components;
 
+use yii\helpers\ArrayHelper;
+use denis909\storage\models\Storage as StorageModel;
+
 class StorageUploadBehavior extends \trntv\filekit\behaviors\UploadBehavior
 {
 
     public $filesStorage = 'storage';
+
+    protected function deleteSingleLog()
+    {
+        $attribute = ArrayHelper::getValue($this->fields(), 'path');
+
+        if ($attribute)
+        {
+            StorageModel::deleteAll([
+                'component' => $this->filesStorage,
+                'path' => $this->owner->{$attribute}
+            ]);
+        }
+    }
+
+    protected function deleteMultipleLog()
+    {
+        foreach($this->fields() as $field)
+        {
+            $attribute = ArrayHelper::getValue($field, 'path');
+
+            StorageModel::deleteAll([
+                'component' => $this->filesStorage,
+                'path' => $this->owner->{$attribute}
+            ]);
+        }
+    }
+
+    public function afterInsertSingle()
+    {
+        parent::afterInsertSingle();
+
+        $this->deleteSingleLog();
+    }
+
+    public function afterInsertMultiple()
+    {
+        parent::afterInsertMultiple();
+
+        $this->deleteMultipleLog();
+    }
+
+    public function afterUpdateSingle()
+    {
+        parent::afterUpdateSingle();
+
+        $this->deleteSingleLog();
+    }
+
+    public function afterUpdateMultiple()
+    {
+        parent::afterUpdateMultiple();
+
+        $this->deleteMultipleLog();
+    }
     
 }
